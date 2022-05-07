36a8e87121e359667fdea12c80c2aba23b4fd27d
juanchuletas <gamj@xanum.uam.mx>
2022-05-07 09:54:45 -0500

    Makefila dld

--- Imgui_Setup/imgui_setup.cpp
+++ Imgui_Setup/imgui_setup.cpp
@@ -53,7 +53,7 @@ void imguiFrameBasic() {
         static float f = 0.0f;
         static int counter = 0;
 
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+        ImGui::Begin("FunGT menu");                          // Create a window called "Hello, world!" and append into it.
 
         ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
         ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
