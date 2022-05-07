6c1af234ecf2cde2c2e81499675959ba429e6851
juanchuletas <gamj@xanum.uam.mx>
2022-05-07 09:27:52 -0500

    imui-setup

---
+++ Imgui_Setup/imgui_setup.hpp
@@ -0,0 +1,9 @@
+#include "../vendor/imgui/imgui.h"
+#include "../vendor/imgui/imgui_impl_glfw.h"
+#include "../vendor/imgui/imgui_impl_opengl3.h"
+
+void imguiSetup(GLFWwindow* window);
+void imguiNewFrame();
+void imguiRender();
+void imguiCleanUp();
+void imguiFrameBasic();
