915d2cfd9268ce15110f5b4d5918c5553a441bb2
juanchuletas <juan.garcia.cpp@gmail.com>
2025-09-30 19:40:17 -0600

    Quaternion

--- Quaternion/quaternion.hpp
+++ Quaternion/quaternion.hpp
@@ -9,10 +9,10 @@ struct Quaternion {
     Quaternion(float w = 1, float x = 0, float y = 0, float z = 0) : w(w), x(x), y(y), z(z) {}
     
     // Create quaternion from axis-angle
-    static Quaternion fromAxisAngle(const fungl::Vec3& axis, float angle) {
+    static Quaternion fromAxisAngle(const fungt::Vec3& axis, float angle) {
         float halfAngle = angle * 0.5f;
         float sinHalf = std::sin(halfAngle);
-        fungl::Vec3 normalizedAxis = axis.normalize();
+        fungt::Vec3 normalizedAxis = axis.normalize();
         
         return Quaternion(
             std::cos(halfAngle),
@@ -42,9 +42,9 @@ struct Quaternion {
     }
     
     // Convert to rotation matrix
-    fungl::Matrix3f toMatrix() const {
+    fungt::Matrix3f toMatrix() const {
 
-        fungl::Matrix3f result;
+        fungt::Matrix3f result;
         
         float xx = x * x, yy = y * y, zz = z * z;
         float xy = x * y, xz = x * z, yz = y * z;
@@ -66,12 +66,12 @@ struct Quaternion {
     }
     
     // Rotate a vector by this quaternion
-    fungl::Vec3 rotateVector(const fungl::Vec3& v) const {
+    fungt::Vec3 rotateVector(const fungt::Vec3& v) const {
         // v' = q * (0, v) * q^(-1)
         Quaternion vecQuat(0, v.x, v.y, v.z);
         Quaternion conjugate(w, -x, -y, -z);
         Quaternion result = (*this) * vecQuat * conjugate;
-        return fungl::Vec3(result.x, result.y, result.z);
+        return fungt::Vec3(result.x, result.y, result.z);
     }
 };
 #endif // _QUATERNION_H_   
