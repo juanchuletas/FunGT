615f8171a2fee25b8ec9aee9d8eea21e7f5dcd9f
juanchuletas <juan.garcia.cpp@gmail.com>
2026-01-05 20:00:20 -0600

    feat: Particle demos

--- vendor/funlib/include/funlib/ParticleSys/ParticleSystem.hpp
+++ vendor/funlib/include/funlib/ParticleSys/ParticleSystem.hpp
@@ -13,7 +13,7 @@ namespace flib{
     {
         class sycl_handler; // Forward declaration of sycl_handler
     public:
-        void static update_ocl(int numOfParticles, unsigned int vbo, Func update_function, T dt){
+        void static update(int numOfParticles, unsigned int vbo, Func update_function, T dt){
             std::size_t n = static_cast<std::size_t>(numOfParticles);
             std::size_t xdim = static_cast<std::size_t>(std::ceil(std::sqrt(n)));
             std::size_t ydim = xdim; 
@@ -29,8 +29,11 @@ namespace flib{
                 throw std::runtime_error("Failed to create OpenCL buffer from GL buffer");
             }
             sycl::context syclCtx = flib::sycl_handler::get_sycl_context();
-            
-            clEnqueueAcquireGLObjects(clqueue, 1, &clbuffer, 0, NULL, NULL);    
+            // CRITICAL: Finish all pending OpenGL operations first
+            glFinish();
+            cl_event acquire_event;
+            clEnqueueAcquireGLObjects(clqueue, 1, &clbuffer, 0, NULL, &acquire_event);
+            clWaitForEvents(1, &acquire_event);  // Wait for acquisition to complete   
             {
                 sycl::buffer<Particle<T>> buf =
                 sycl::make_buffer<sycl::backend::opencl, Particle<T>>(clbuffer, syclCtx);
@@ -49,47 +52,15 @@ namespace flib{
                 });
 
                  Q.wait();
-
-                  // FORCE SYNC: Just create host accessor to trigger sync, but don't copy
-                {
-                    auto host_acc = buf.template get_access<sycl::access::mode::read>();
-                    // Just creating the accessor forces the sync back to OpenCL buffer
-                    // No need to actually use host_acc or copy data
-                    (void)host_acc; // Suppress unused variable warning
-                }
             }
              clFinish(clqueue);
             //Release OpenCL buffer
-            clEnqueueReleaseGLObjects(clqueue, 1, &clbuffer, 0, NULL, NULL);
-            clFinish(clqueue);
+            cl_event release_event;
+            clEnqueueReleaseGLObjects(clqueue, 1, &clbuffer, 0, NULL, &release_event);
+            clWaitForEvents(1, &release_event);  // Wait for release to complete
 
             glMemoryBarrier(GL_VERTEX_ATTRIB_ARRAY_BARRIER_BIT | GL_BUFFER_UPDATE_BARRIER_BIT);
             clReleaseMemObject(clbuffer);
-        }
-    public:
-    
-        void static update(ParticleSet<T> &particles, Func update_function, T dt){
-
-            std::size_t n = particles._particles.size();
-            std::size_t xdim = static_cast<std::size_t>(std::ceil(std::sqrt(n)));
-            std::size_t ydim = xdim;    
-            sycl::queue Q = flib::sycl_handler::get_queue();
-            {
-               
-                sycl::buffer<Particle<T>> buf(particles._particles.data(), sycl::range<1>(n));
-                Q.submit([&](sycl::handler &cgh) {
-                auto acc = buf.template get_access<sycl::access::mode::read_write>(cgh);
-                    cgh.parallel_for(sycl::range<2>(sycl::range<2> {static_cast<size_t>(xdim),static_cast<size_t>(ydim)}),[=](sycl::item<2> item) {
-                    //user defined lambda function
-                    std::size_t index = item[0] * ydim + item[1];
-                    if (index < n) {
-                        update_function(acc[index], dt);
-                    }
-                    });
-                 });
-
-            }
-
         }
     
     };
